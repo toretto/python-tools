d6f55c3a572adc7355eb06d392ffe5c18bfeef48
Steven Zeegers <nick_toretto@hotmail.com>
2023-04-22 17:32:05 +0200

    Using CSV as source-file for status checker

---
+++ Web/Website-Status-Checker/readme.md
@@ -0,0 +1,4 @@
+#Instructions
+- Create a file named "db.csv" in the folder
+- Add the website you want to add and the "heartbeat", which is the text to look for on the page, separated by a ":"
+- Run the script
--- Web/Website-Status-Checker/website.py
+++ Web/Website-Status-Checker/website.py
@@ -1,15 +1,22 @@
 # This Python script checks if a website is online and whether the heartbeat phrase is found. 
 import requests
+import csv
 
-url = "https://stevenzeegers.me"
-r = requests.get(f'{url}')
 
-if r.status_code == 200: 
-    print (f'{url}is alive. Checking heearbeat code.')
+with open('db.csv') as csvfile:
+    csvreader = csv.reader(csvfile,delimiter=";")
 
-    if "Steven Blogs" in r.text: 
-        print (f"{url} heartbeat found.")
-    else:
-        print ("Heartbeat not found. Check DB connection.")
-else: 
-    print (f('{url} appears to be down.'))
+    for row in csvreader: 
+        url = row[0]
+        heartbeat = row[1]
+        r = requests.get(f'{url}')
+
+        if r.status_code == 200: 
+            print (f'{url}is alive. Checking heearbeat code.')
+
+            if  heartbeat in r.text: 
+                print (f"{url} heartbeat found.")
+            else:
+                print ("Heartbeat not found. Check DB connection.")
+        else: 
+            print (f('{url} appears to be down.'))
