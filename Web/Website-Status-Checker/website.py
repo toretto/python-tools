e29a1466771b30ce78f898ce2d290ff521e73444
Steven Zeegers <nick_toretto@hotmail.com>
2023-05-08 21:40:49 +0200

    Style fixes
    Added a space. Added colors when succesfull. Had existential dread

--- Web/Website-Status-Checker/website.py
+++ Web/Website-Status-Checker/website.py
@@ -16,12 +16,12 @@ with open('db.csv') as csvfile:
         heartbeat = row[1]
         r = requests.get(f'{url}')
         print (r.status_code)
-#        if r.status_code == 200: 
-#            print (f'{bcolors.SUCC}{url} is alive. Checking heearbeat code.{bcolors.ENDC}')
-#
-#            if  heartbeat in r.text: 
-#                print (f"{bcolors.SUCC}{url} heartbeat found. {bcolors.ENDC}\n")
-#            else:
-#                print (f"{bcolors.FAIL}Heartbeat not found. Check DB connection.{bcolors.ENDC} \n")
-#        else: 
-#            print (f('{bcolors.FAIL}{url} appears to be down.{bcolors.ENDC}'))
+        if r.status_code == 200: 
+            print (f'{bcolors.SUCC}{url} is alive. Checking heearbeat code.{bcolors.ENDC}')
+
+            if  heartbeat in r.text: 
+                print (f"{bcolors.SUCC}{url} heartbeat found. {bcolors.ENDC}\n")
+            else:
+                print (f"{bcolors.FAIL}Heartbeat not found. Check DB connection.{bcolors.ENDC} \n")
+        else: 
+            print (f('{bcolors.FAIL}{url} appears to be down.{bcolors.ENDC}'))
