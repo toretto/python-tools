7cb69176cb303fb1f4ba5601e153477cde3987ef
Steven Zeegers <nick_toretto@hotmail.com>
2023-05-28 15:48:05 +0200

    Resolving hostname
    Script now tries to resolve the hostname before trying to connect, so we don't get a nasty error. CSV requires another field because reasons.

--- Web/Website-Status-Checker/website.py
+++ Web/Website-Status-Checker/website.py
@@ -2,20 +2,30 @@
 from re import X
 import requests
 import csv
+import socket
 
 class bcolors:
     SUCC = '\033[92m'
     ENDC = '\033[0m'
     FAIL = '\x1b[31m'
 
+print("\nStarting host scans. \n")
+print("#RESULTS \n")
+
 with open('db.csv') as csvfile:
     csvreader = csv.reader(csvfile,delimiter=";")
 
     for row in csvreader: 
-        url = row[0]
-        heartbeat = row[1]
+        hostname = row [0]
+        url = row[1]
+        heartbeat = row[2]
+        try:
+            o = socket.gethostbyname(hostname)
+        except:
+            print (f"{bcolors.FAIL}Can\'t resolve the hostname {hostname}, skipping.{bcolors.ENDC}\n")
+            continue
         r = requests.get(f'{url}')
-        print (r.status_code)
+        # print (r.status_code)
         if r.status_code == 200: 
             print (f'{bcolors.SUCC}{url} is alive. Checking heearbeat code.{bcolors.ENDC}')
 
@@ -24,4 +34,4 @@ with open('db.csv') as csvfile:
             else:
                 print (f"{bcolors.FAIL}Heartbeat not found. Check DB connection.{bcolors.ENDC} \n")
         else: 
-            print (f('{bcolors.FAIL}{url} appears to be down.{bcolors.ENDC}'))
+            print (f"{bcolors.FAIL}{url} appears to be down.\nNo heartbeat for obvious reasons.{bcolors.ENDC}")
